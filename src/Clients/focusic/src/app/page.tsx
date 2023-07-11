127108bf36f13ca28cb73e7d08d0e21f3a332837
jaywong012 <125846034+jaywong012@users.noreply.github.com>
2023-07-11 23:17:18 +0700

    Update page.tsx

--- src/Clients/focusic/src/app/page.tsx
+++ src/Clients/focusic/src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page(){
-    return <h1>Hello, Dashboard Page!</h1>
+import Image from "next/image";
+import ButtonLink from "@components/ButtonLink/ButtonLink";
+
+export default function Page() {
+  return(
+  <>
+    <h1>Hello, Home page!</h1>
+    <ButtonLink/>
+  </>
+  )
 }
