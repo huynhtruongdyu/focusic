c5576f50107e141972238f648c680dfc07b5776c
jaywong012 <125846034+jaywong012@users.noreply.github.com>
2023-07-11 23:10:34 +0700

    [FE] Alter front-end to focusic, add navigations

---
+++ src/Clients/focusic/public/navigations/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navigation = {
+    HomePage: {
+        name: "Home Page",
+        link: "/"
+    },
+    SettingPage: {
+        name: "Setting Page",
+        link: "/dashboard/settings"
+    }
+}
+
+export default navigation
